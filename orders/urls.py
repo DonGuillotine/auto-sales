855c619eabd33009e6c8fbc00ffd0c3cd223be96
DonGuillotine <infect3dlab@gmail.com>
2022-12-23 10:33:40 +0100

    Revert "Making Updates to Language"
    This reverts commit ef207f118eb8e9d2b303c524c40439875e59c547.

--- orders/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('place_order/', views.place_order, name='place_order'),
-    path('payments/', views.payments, name='payments'),
-    path('order_complete',  views.order_complete, name='order_complete'),
-]
--- store/cdn/backends.py
+++
@@ -1,9 +0,0 @@
-from storages.backends.s3boto3 import S3Boto3Storage
-
-
-class StaticRootS3Boto3Storage(S3Boto3Storage):
-    location = 'static'
-
-
-class MediaRootS3Boto3Storage(S3Boto3Storage):
-    location = 'media'
